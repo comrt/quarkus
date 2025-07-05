71432594db791394a18f85395d9c239d876867e1
comrt <comrt.dev@gmail.com>
2025-07-05 16:11:53 +0200

    Add header params to MultipartFormDataOutput and FileUpload closes quarkus#48709

--- extensions/resteasy-reactive/rest-client/deployment/src/test/java/io/quarkus/rest/client/reactive/multipart/PathFileDownload.java
+++ extensions/resteasy-reactive/rest-client/deployment/src/test/java/io/quarkus/rest/client/reactive/multipart/PathFileDownload.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import jakarta.ws.rs.core.MultivaluedMap;
-
 import org.jboss.resteasy.reactive.multipart.FileDownload;
 
 public class PathFileDownload implements FileDownload {
